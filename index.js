fa6f2c96949d2f14f7fcd8c7a3eb05f333d36a25
pm <Mai_dth235696@student.agu.edu.vn>
2026-04-14 01:38:09 +0700

    138

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+    callbackURL: "https://one4pm-tour.onrender.com/auth/google/callback"
 },
     async function (accessToken, refreshToken, profile, cb) {
         try {
